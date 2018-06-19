2c3e040799c88223778a6fd307e75f6bb11b55de
Justin MacCallum <justin.maccallum@ucalgary.ca>
2018-06-18 21:46:46 -0600

    Updated build scripts

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -1,17 +1,7 @@
 #!/bin/bash
 set -e
-# Activate Holy Build Box environment.
-source /hbb_exe/activate
-
-# Disable PYTHONPATH
-unset PYTHONPATH
-
 set -x
-curl -s -O https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
-bash Miniconda3-latest-Linux-x86_64.sh -b -p /anaconda
-PATH=/opt/rh/devtoolset-2/root/usr/bin:/anaconda/bin:$PATH
-conda config --add channels omnia
-conda config --add channels maccallum_lab
-conda install -yq conda-build=2.1.17 jinja2 anaconda-client
 
-/io/conda-build-all --force $UPLOAD --python $PYVER -- /io/openmm /io/meld
+conda config --add channels omnia
+conda config --add channels conda-forge
+/io/conda-build-all -vvv --force $UPLOAD --python $PYVER -- /io/openmm /io/meld
--- openmm/run_test.py
+++ openmm/run_test.py
@@ -3,7 +3,7 @@
 from simtk import openmm
 
 # Check major version number
-assert openmm.Platform.getOpenMMVersion() == '7.1', "openmm.Platform.getOpenMMVersion() = %s" % openmm.Platform.getOpenMMVersion()
+assert openmm.Platform.getOpenMMVersion() == '7.2.2', "openmm.Platform.getOpenMMVersion() = %s" % openmm.Platform.getOpenMMVersion()
 
 # Check git hash
-assert openmm.version.git_revision == '1e5b258c0df6ab8b4350fd2c3cbf6c6f7795847c', "openmm.version.git_revision = %s" % openmm.version.git_revision
+assert openmm.version.git_revision == '32bc79aa35da5495c9947ba817cb956964d63a4b', "openmm.version.git_revision = %s" % openmm.version.git_revision
