97b7a5010f567dea8b124fe6b34e66541dad13eb
Justin MacCallum <justin.maccallum@ucalgary.ca>
2018-07-13 16:30:13 -0600

    Update docker-build.sh

--- devtools/docker-build.sh
+++ devtools/docker-build.sh
@@ -2,7 +2,6 @@
 set -e
 set -x
 
-conda-config --add channels maccallum_lab
 conda config --add channels omnia
 conda config --add channels conda-forge
 /io/conda-build-all -vvv --force $UPLOAD --python $PYVER -- /io/openmm /io/meld
