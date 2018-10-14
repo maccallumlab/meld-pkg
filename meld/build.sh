4fa2cda094201e2b4be11f3702ae4669c21bb1f1
Justin MacCallum <justin.maccallum@ucalgary.ca>
2018-10-14 12:23:37 -0600

    Update directory for documentation

--- meld/build.sh
+++ meld/build.sh
@@ -47,9 +47,11 @@ fi
 
 mkdir build
 cd build
-
 cmake .. $CMAKE_FLAGS
 make -j$CPU_COUNT all
 make -j$CPU_COUNT install PythonInstall
 make -j$CPU_COUNT DoxygenApiDocs
-aws s3 sync --region us-west-2 --delete /anaconda/conda-bld/work/docs/_build/html/ s3://meldmd.org/
+cd ..
+cd ..
+
+aws s3 sync --region us-west-2 --delete `pwd`/docs/_build/html s3://meldmd.org/
